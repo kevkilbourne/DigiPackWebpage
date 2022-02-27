008f25380edcb559e85d73e9d1184c4e7b7174fe
Jake <jakenovin3>
2022-02-27 16:20:46 -0700

    Added database models (in models.py) for a basic class

--- DigitalBackpack/models.py
+++ DigitalBackpack/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Class(models.Model):
+    # The name of the class is a String
+    classname = models.CharField(max_length=50)
+
+    # Makes the instructor a 'User' and on deletion of the class, the instructor
+    # is also deleted ('on_delete=models.CASCADE')
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # When a class is called in Python shell, it will display the Class's classname
+    def __str__(self):
+        return self.classname
+
