4d7e948ba710aab8fe47264cb6f5957ec6e6c1ae
Jake <jakenovin3>
2022-03-07 20:52:44 -0700

    "Online Connectivity" button now redirects to .../student/connectivity/ and displays heatmap_timeframe.png. Still need to make this webpage look better w/ CSS

--- DigitalBackpack/urls.py
+++ DigitalBackpack/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( 'student/', views.studentpage, name='student_page' ),
-    path( 'teacher/', views.teacherpage, name='teacher_page' ),
+    path( 'student/', views.student_page, name='student_page' ),
+    path( 'teacher/', views.teacher_page, name='teacher_page' ),
+    path( 'student/connectivity/', views.connection_page, name='connection_timeframe_page' ),
 ]
