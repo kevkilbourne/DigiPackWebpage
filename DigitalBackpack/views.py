79b26538e3ce66cc3a9edfd452844aa32b59b7f4
Kevin Kilbourne <kevkilbourne@gmail.com>
2022-03-08 19:22:54 -0700

    Fully integrated MVPs for FRs
    Fully integrated code for:
    Website rating
    Student viewing of connectivity

--- DigitalBackpack/views.py
+++ DigitalBackpack/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 import sqlite3
 import importlib
+import numpy as np
+import matplotlib.pyplot as plt, mpld3
+import seaborn as sns
+from pandas import read_csv
 import DigitalBackpack.models as models
 from .forms import RatingForm 
 
-def studentpage(request):
+def student_page(request):
     return render(request, 'DigitalBackpack/StudentWebpage.html')
 
-def teacherpage(request):
+def teacher_page(request):
     return render(request, 'DigitalBackpack/TeacherWebpage.html')
 
 
@@ -92,3 +96,47 @@ def searching_algorithm(request):
            index += 1
 
     return render(request, 'DigitalBackpack/TeacherWebpage.html')
+
+def connection_page(request):
+    # Open .csv file and read
+    dataset = read_csv("DigitalBackpack/static/csv/timeframes.csv")
+    # print(dataset)
+
+    # Sets up general heatmap attributes including size, title, and x and y axes
+    plt.figure(figsize=(8, 8))
+    # plt.title("Your Recent Connections")
+    plt.xlabel("Days of the week", size=15)
+    plt.ylabel("Time of day", size=15)
+
+    # Creation of the heatmap with specific characteristics
+    heatmap = sns.heatmap(dataset, linewidths=0.5, square=True, cmap=["#5d7682", "#38b6ff"], cbar=False)
+
+    # Adjustments made to the heatmap's characteristics
+    time_ticks = ["12:00AM", "1:00AM", "2:00AM", "3:00AM", "4:00AM", "5:00AM", "6:00AM", "7:00AM", "8:00AM", "9:00AM",
+                  "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM",
+                  "8:00PM", "9:00PM", "10:00PM", "11:00PM"]
+    heatmap.invert_yaxis()
+    plt.yticks(np.arange(24), time_ticks)  # Arranges 24 ticks and labels them with corresponding value in 'time_ticks'
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=0)
+
+    # ----------------- Getting heatmap to browser ----------------- #
+
+    # plt.savefig - Saves the figure as specified format for later usage
+    complete_file = "DigitalBackpack/static/img/heatmap_timeframe.png"
+    plt.savefig(complete_file, format='png')
+
+    # mpld3.show() - Open figure in a web browser
+    # Similar behavior to plt.show(). This opens the D3 visualization of the specified figure in the web browser.
+    # On most platforms, the browser will open automatically
+    # mpld3.show(heatmap, 'localhost')
+
+    # mlpd3.save_html() - Saves a matplotlib figure to a html file
+    # mpld3.save_html(heatmap, "timeframe_heatmap.html")
+
+    # mlpd3.fig_to_html() - Outputs html representation of the figure
+    # html_str = mpld3.fig_to_html(plt)
+    # html_file = open("index.html", "w")
+    # html_file.write(html_str)
+
+    return render(request, 'DigitalBackpack/student_connectivity.html')
