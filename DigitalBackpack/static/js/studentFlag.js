80fe31f50bb9204d5fda16c3f413ae7cc190b209
Nick Caporusso <62626744+nickcappy@users.noreply.github.com>
2022-04-19 15:37:42 -0700

    Update studentFlag.js

--- DigitalBackpack/static/js/studentFlag.js
+++ DigitalBackpack/static/js/studentFlag.js
@@ -5,13 +5,7 @@ function swapFlag(event, element)
   if (element.className == "notFlagged")
   {
     element.className = "flagged";
-/*
-    Model.objects.filter(id = notFlagged).update(field1 = flagged);
 
-    Student.flagged = "flagged";
-    Student.save();
-
-*/
     alert("Student has been successfully flagged.");
 
     let teachInput = prompt("Would you like to send an email to the student's parents/guardians (yes or no)?").toLowerCase();
@@ -19,10 +13,7 @@ function swapFlag(event, element)
     switch (teachInput)
     {
       case "yes":
-        let sendTo = prompt("Who is this email going to (ex: john.doe@gmail.com)?");
-        let userMessage = prompt("Write your email's message here.");
-
-        sendEmail(sendTo, userEmail);
+        window.open('https://mail.google.com/mail/?view=cm&fs=1','_blank');
         break;
 
       case "no":
@@ -38,34 +29,10 @@ function swapFlag(event, element)
   else
   {
     element.className = "notFlagged";
-/*
-    Model.objects.filter(id = flagged).update(field1 = notFlagged);
 
-    Student.flagged = "notFlagged";
-    Student.save();
-
-*/
     // prompt user that it was successful
     alert("Student has been successfully unflagged.");
 
     return element;
   }
-}
-
-function sendEmail(sendTo, message)
-{
-  Email.send(
-    {
-      Host: "www.gmail.com",
-      Username: "",
-      Password: "",
-      To: sendTo,
-      From: User.objects.get(id=request.session.get('_auth_user_id')).email,
-      Subject: "Regarding Your Students Activity in Class",
-      Body: message,
-    })
-    .then(function (message)
-    {
-      alert("Email sent successfully");
-    });
 }
