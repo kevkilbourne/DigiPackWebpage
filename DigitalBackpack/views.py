d0171ca5b091c7d4e212cc417e872e8437deb1ee
Jon-Nation <jdn238@nau.edu>
2022-04-21 12:44:33 -0700

    Update views.py

--- DigitalBackpack/views.py
+++ DigitalBackpack/views.py
@@ -611,7 +611,9 @@ def view_student(request):
     student_heatmap_csv = student_heatmap_path + '/student_' + student_username + '_heatmap.csv'
 
     # This grabs the studentID that was sent from the button press in the Teacher Webpage
-    currentStudentID = request.POST['studentID']
+    currentStudentID = int(request.POST['studentID'])
+    print("CURRENTSTUDENTID")
+    print(currentStudentID)
     # currentStudentID = 1 # CHANGE (for testing purposes)
 
     # This checks to see if the currentStudentID is larger than the total number of students in model.py object.
@@ -688,7 +690,7 @@ def view_myself(request):
     semester_heatmap_csv = student_heatmap_path + '/' + student_username + '_semester_heatmap.csv'
 
     # This grabs the studentID that was sent from the button press in the Teacher Webpage
-    currentStudentID = request.POST['studentID']
+    currentStudentID = int(request.POST['studentID'])
     # currentStudentID = 1 # CHANGE (for testing purposes)
 
     # This checks to see if the currentStudentID is larger than the total number of students in model.py object.
